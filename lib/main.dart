a5e2ec6192b02345d9b22d9f201d6c9cc59f0ede
Shuangbing <he.shuangbing@gmail.com>
2020-01-02 14:36:37 +0900

    init project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'header.dart';
+import 'footer.dart';
+
+void main() {
+ runApp(App());
+}
+
+class App extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     debugShowCheckedModeBanner: false,
+     home: Scaffold(
+       appBar: Header(),
+       body: Center(child: Text("追加ボタンを押して手取りを算出しましょう")),
+     ),
+   );
+ }
+}
